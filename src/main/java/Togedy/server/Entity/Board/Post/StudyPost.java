63f17e92b55e5ff0b9fd812d60703dfbae81d0cd
arkchive <kyungminkim7@konkuk.ac.kr>
2024-10-14 14:20:56 +0900

    feat: 공부인증게시판 게시글 조회 및 생성 api

--- src/main/java/Togedy/server/Entity/Board/Post/StudyPost.java
+++ src/main/java/Togedy/server/Entity/Board/Post/StudyPost.java
@@ -1,11 +1,20 @@
 package Togedy.server.Entity.Board.Post;
 
+import Togedy.server.Entity.Board.PostImage;
+import Togedy.server.Entity.User.User;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("STUDY")
 @NoArgsConstructor
 public class StudyPost extends Post {
+    @Builder
+    public StudyPost(User user, String title, String content, List<PostImage> postImages) {
+        super(user, title, content, postImages);
+    }
 }
