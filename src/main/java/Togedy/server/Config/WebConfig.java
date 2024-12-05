8ef3d4c7f53de366493c4a41d275a4e8b22ba74c
arkchive <kyungminkim7@konkuk.ac.kr>
2024-12-05 16:11:54 +0900

    feat: WebConfig 추가

---
+++ src/main/java/Togedy/server/Config/WebConfig.java
@@ -0,0 +1,27 @@
+package Togedy.server.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173", "https://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .exposedHeaders("location", "Authorization")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
